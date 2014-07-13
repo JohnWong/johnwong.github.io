2d3498276bb5b714ed8886ea7dadc0b8c4bd8a2a
John Wong <huangxiaozhe1988@gmail.com>
2014-07-13 22:47:11 +0800

    add template build system

---
+++ build.py
@@ -0,0 +1,47 @@
+import json
+import codecs
+
+
+class BlogBuiler:
+
+    def __init__(self, name="config.json", dir="template"):
+        self.dir = dir
+        self.config = json.loads(self.readfile(name))
+        pass
+
+    def buildtemplate(self):
+        self._temp = {}
+        build_config = self.config['template']
+        for build_item in build_config:
+            result = self.buildhtml(build_item['config'])
+            self._temp[build_item['id']] = result
+            if build_item['output']:
+                with codecs.open(build_item['id'], 'w', encoding="utf-8") as f:
+                    f.write(result)
+        pass
+
+    def readfile(self, file_name):
+        with codecs.open(file_name, 'r',  encoding='utf-8') as f:
+            return f.read()
+        pass
+
+    def buildhtml(self, config):
+        result = self.readfile(self.dir + '/' + config['base'])
+        if config.get('script'):
+            scripts = ''
+            for script in config.get('script'):
+                scripts += '<script src="' + script + '"></script>'
+            result = result.replace('${script}', scripts)
+            pass
+        for (inject_id, inject_config) in config['inject'].items():
+            if self._temp.get(inject_config):
+                inject_content = self._temp.get(inject_config)
+            else:
+                inject_content = self.readfile(self.dir + '/' + inject_config)
+            result = result.replace(u'${' + inject_id + '}', inject_content)
+        return result
+
+
+if __name__ == "__main__":
+    blogBuiler = BlogBuiler()
+    blogBuiler.buildtemplate()
---
+++ js/article.js
@@ -0,0 +1,55 @@
+$(document).on("scroll", function() {
+    $(this).scrollTop() > 25 ? $("#top").addClass("scrolled") : $("#top").removeClass("scrolled");
+});
+$(document).ready(function() {
+    var params = function() {
+        for (var t, e = [], o = window.location.href.slice(window.location.href.indexOf("?") + 1).split("&"), r = 0; r < o.length; r++)
+            t = o[r].split("="), e.push(t[0]), e[t[0]] = t[1];
+        return e
+    }();
+    var cat = params['cat'], id = params['id'];
+    if (!cat || !id) {
+        $(".post-big-title").html('未找到该文章');
+        return;
+    };
+    id = decodeURI(id), cat = decodeURI(cat)
+    $(".post-big-title").html(id);
+    $.get('data/list.json', function(data) {
+        if (data[cat] && data[cat][id]) {
+            var item = data[cat][id];
+            var author = item['author'];
+            var time = item['time'];
+            var cover_url = item['cover_url'];
+            $(".post-poster").html('<img src="' + cover_url + '" alt="' + id + '">');
+            var icon;
+            switch (params['cat']) {
+                case "Mobile":
+                    icon = 'glyphicon-phone';
+                    break;
+                case "Life":
+                    icon = 'glyphicon-home';
+                    break;
+                    // case "Bookmarks": icon = 'glyphicon-bookmark'; break;
+                case "Travel":
+                    icon = 'glyphicon-road';
+                    break;
+                    // case "White Hat": icon = 'glyphicon-lock';break;
+                case "Front End":
+                    icon = 'glyphicon-link';
+                    break;
+            };
+            var content = '<a class="category" href="cat.html?id=' + cat + '"><span class="glyphicon ' + icon + '"></span>' + params['cat'] + '</a> | <span id="author" class="author">' + author + '</span> • ' + time;
+            $(".post-author").html(content);
+
+        };
+    });
+    $.get('data/' + cat + '/' + id + '.md', function(data) {
+        $('#preview').html(marked(data));
+        $('#preview code.highlight').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+    });
+    $.get('template/nav-right.html', function(data) {
+        $('#latest-posts').html(data);
+    });
+});
